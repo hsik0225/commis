d91226c2596965f1a195d3419962083d7e8f727e
GitHub Action <action@github.com>
2023-07-23 10:27:23 +0000

    Template cleanup

--- src/main/kotlin/com/github/hsik0225/commis/MyBundle.kt
+++ src/main/kotlin/com/github/hsik0225/commis/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.hsik0225.commis
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/hsik0225/commis/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/hsik0225/commis/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.hsik0225.commis.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
